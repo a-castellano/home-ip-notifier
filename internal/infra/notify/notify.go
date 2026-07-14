939eeec5fd95fe5e6f3a9e8dceb2bbd192821288
Álvaro Castellano <alvaro@windmaker.net>
2026-07-14 20:28:38 +0200

    Write notificator

--- internal/infra/notify/notify.go
+++ internal/infra/notify/notify.go
@@ -1,1 +1,59 @@
 package notify
+
+import (
+	"context"
+
+	logger "github.com/a-castellano/go-services/infra/logger"
+	"github.com/a-castellano/go-services/infra/smtp"
+	"github.com/a-castellano/go-services/services/notificator"
+	notification "github.com/a-castellano/go-types/types/notification"
+	smtpconfig "github.com/a-castellano/go-types/types/smtp"
+)
+
+type MailNotificator struct {
+	driver      *notificator.Notificator
+	destination string
+}
+
+func NewNotificator(ctx context.Context, config *smtpconfig.Config, destination string) *MailNotificator {
+	log := logger.FromContext(ctx).With("operation", "NewNotificator")
+
+	log.DebugContext(ctx, "creating notificator")
+
+	sender := smtp.NewSender(config, smtp.TLSDialer{})
+	mailSender := smtp.NewMailSender(sender)
+
+	mailDriver := notificator.NewNotificator(mailSender)
+
+	return &MailNotificator{driver: mailDriver, destination: destination}
+}
+
+func (n *MailNotificator) Notify(ctx context.Context, title string, message string) error {
+	log := logger.FromContext(ctx).With("operation", "Notify")
+
+	log.DebugContext(ctx, "prepare new notification")
+
+	newMessage, newMessageError := notification.NewNotification(
+		n.destination,
+		title,
+		message,
+		notification.WithLevel(notification.Info),
+	)
+
+	if newMessageError != nil {
+		errorString := "failed to create a new notification"
+		log.ErrorContext(ctx, errorString, "error", newMessageError)
+		return newMessageError
+	}
+	// This log is already emitted by driver
+	//log.DebugContext(ctx, "sending notification", "notification", newMessage)
+
+	notifyError := n.driver.Notify(ctx, newMessage)
+	if notifyError != nil {
+		errorString := "failed to send notification"
+		log.ErrorContext(ctx, errorString, "error", notifyError)
+		return notifyError
+	}
+
+	return nil
+}
